5547a2b6bea9c1f513a2fb3366430ecc992bc4d3
abdulah <abdullahalhoothy92@gmail.com>
2024-03-18 00:07:01 -0400

    updated repo structure

--- aws/variables.tf
+++ aws/variables.tf
@@ -1,3 +1,7 @@
+# i have secrets stored in aws\secrets.auto.tfvars as below
+#  access_key = "********************"
+#  secret_key = "*******************************************"
+
 variable "access_key" {
   description = "AWS Access Key"
   type        = string
